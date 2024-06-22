6bbafb9522eca6267d568a1e14aff6a0b8e46e09
tahmim al ashik <tahmim0006@gmail.com>
2024-06-22 18:01:47 +0600

    project has created for profile app

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProfileController extends Controller
+{
+    public function index($id)
+    {
+        $name = "Donald Trump";
+        $age = "75";
+
+        $data = [
+            'id' => $id,
+            'name' => $name,
+            'age' => $age
+        ];
+
+        $cookieName = 'access_token';
+        $cookieValue = '123-XYZ';
+        $cookieMinutes = 1;
+        $cookiePath = '/';
+        $cookieDomain = $_SERVER['SERVER_NAME'];
+        $cookieSecure = false;
+        $cookieHttpOnly = true;
+
+        $cookie = cookie($cookieName, $cookieValue, $cookieMinutes, $cookiePath, $cookieDomain, $cookieSecure, $cookieHttpOnly);
+
+        return response()->json($data, 200)->cookie($cookie);
+    }
+}
